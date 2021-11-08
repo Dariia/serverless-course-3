a84d3b282627ac468dc881edae8e78fbd29f0b8f
Dariia Drobotko <drobotcko@gmail.com>
2021-11-09 00:41:46 +0200

    Initial commit

---
+++ test/serverless_course_3.1.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const ServerlessCourse31 = require('../lib/serverless_course_3.1-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless_course_3.1-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ServerlessCourse31.ServerlessCourse31Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
